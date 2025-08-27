a4ed05591909dc895c8b232d68f564743966c5e4
shubh <shubh1122sf@gmail.com>
2025-08-27 17:47:33 +0000

    TMKB KITNI BAAR KARYEGA

--- next.config.js
+++ next.config.js
@@ -16,12 +16,6 @@ const nextConfig = {
       }
     ],
   },
-  env: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 module.exports = nextConfig;
