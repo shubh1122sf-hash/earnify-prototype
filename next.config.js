07798cbe366d5c5ed543a89367903c610f462e21
shubh <shubh1122sf@gmail.com>
2025-08-27 17:44:35 +0000

    BRO STILL DOSENT WORK WHAT IM SUPPOSED TO DO WITH YOU

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
 
