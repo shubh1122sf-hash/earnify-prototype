994d6692c0249dea591809b22026b0b4e30be78c
shubh <shubh1122sf@gmail.com>
2025-08-27 17:42:17 +0000

    File changes

--- next.config.js
+++ next.config.js
@@ -16,6 +16,11 @@ const nextConfig = {
       }
     ],
   },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 module.exports = nextConfig;
